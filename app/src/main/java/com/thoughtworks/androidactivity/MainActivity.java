9571da2be6f65335f419ecb345bc54eaebb6928e
unknown <hzh_innovation@163.com>
2020-08-13 15:00:45 +0800

    [hzh] feat: add click listener to button

--- app/src/main/java/com/thoughtworks/androidactivity/MainActivity.java
+++ app/src/main/java/com/thoughtworks/androidactivity/MainActivity.java
@@ -2,13 +2,28 @@ package com.thoughtworks.androidactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    private View.OnClickListener jumpToSelectConnectView = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(MainActivity.this, MainActivity.class);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btn_activity_practices);
+        button.setOnClickListener(jumpToSelectConnectView);
     }
 }
